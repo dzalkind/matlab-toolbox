6ce2a1fed162432c3287b82145e57b44ca60e9ae
dzalkind <dzalkind@nrel.gov>
2020-07-24 14:25:23 -0600

    Optionally read FAST inputs

--- FAST2MATLAB/FASTout2Matlab.m
+++ FAST2MATLAB/FASTout2Matlab.m
@@ -12,68 +12,69 @@ POST_PROCESS = 1;
 
 %% Input Processing
 
-fast.FAST_InputFile    = 'steady_0';   % FAST input file (no ext)
-fast.FAST_directory    = '/Users/dzalkind/Tools/SaveData/SweepPtfmMass/Steady/';   % Path to fst directory files
+fast.FAST_InputFile    = 'lin_2';   % FAST input file (no ext)
+fast.FAST_directory    = '/Users/dzalkind/Tools/SaveData/TrimTest/LinearPitch';   % Path to fst directory files
 
 
 %% Read
-FP = FAST2Matlab(fullfile(fast.FAST_directory,[fast.FAST_InputFile,'.fst']),2); %FP are Fast Parameters, specify 2 lines of header (FAST 8)
-
-[EDP, EDFile]   = GetFASTPar_Subfile(FP, 'EDFile', fast.FAST_directory, fast.FAST_directory);
-[IWP, IWFile]   = GetFASTPar_Subfile(FP, 'InflowFile', fast.FAST_directory, fast.FAST_directory);
-[ADP, ADFile]   = GetFASTPar_Subfile(FP, 'AeroFile', fast.FAST_directory, fast.FAST_directory);
-[SvDP, SvDFile] = GetFASTPar_Subfile(FP, 'ServoFile', fast.FAST_directory, fast.FAST_directory);
-[HDP, HDFile]   = GetFASTPar_Subfile(FP, 'HydroFile', fast.FAST_directory, fast.FAST_directory,true);
-% [SbDP, SbDFile] = GetFASTPar_Subfile(FP, 'SubFile', fast.FAST_directory, fast.FAST_directory);
-% [MDP, MDFile]   = GetFASTPar_Subfile(FP, 'MooringFile', fast.FAST_directory, fast.FAST_directory);
-% [IDP, IDFile] = GetFASTPar_Subfile(FP, 'IceFile', fast.FAST_directory, fast.FAST_directory);
-
-% Get & Set Blade Files
-% ED_BldFile              = GetFASTPar(EDP, 'BldFile1');
-[ED_BldP, ED_BldFile]   = GetFASTPar_Subfile(EDP, 'BldFile1', fast.FAST_directory, fast.FAST_directory);
-
-% Get & Set Tower File
-[ED_TwrP, ED_TwrFile]   = GetFASTPar_Subfile(EDP, 'TwrFile', fast.FAST_directory, fast.FAST_directory);
-
-% AeroDyn Blade
-[AD_BldP, AD_BldFile]   = GetFASTPar_Subfile(ADP, 'ADBlFile(1)', fast.FAST_directory, fast.FAST_directory);
-
-% HydroData
-% PotFile                 = GetFASTPar(HDP,'PotFile');
-
-% Airfoils
-
-% Control Parameters
-[~,SD_dllFile]          = GetFASTPar_Subfile(SvDP,'DLL_InFile',fast.FAST_directory, fast.FAST_directory);
-SD_dllP                 = ROSCO2Matlab(SD_dllFile,2);
-
-CpCtCqFile              = GetFASTPar(SD_dllP,'!PerfFileName');
-%%% TODO: add processing here to look at/use Cx tables
-
-% MoorDyn: let's just copy for meow
-MDFile = GetFASTPar(FP,'MooringFile');
-
-
-
-
-
-%% Write Outputs and Cleanup
-
-% parameters
-P.FP        = FP;
-P.EDP       = EDP;
-P.IWP       = IWP;
-P.ADP       = ADP;
-P.HDP       = HDP;
-P.SvDP      = SvDP;
-P.ED_BldP   = ED_BldP;
-P.ED_TwrP   = ED_TwrP;
-P.AD_BldP   = AD_BldP;
-P.SD_dllP   = SD_dllP;
-
-clearvars FP EDP IWP ADP HDP SvDP ED_BldP ED_TwrP AD_BldP SD_dllP
-
-
+if 0
+    FP = FAST2Matlab(fullfile(fast.FAST_directory,[fast.FAST_InputFile,'.fst']),2); %FP are Fast Parameters, specify 2 lines of header (FAST 8)
+    
+    [EDP, EDFile]   = GetFASTPar_Subfile(FP, 'EDFile', fast.FAST_directory, fast.FAST_directory);
+    [IWP, IWFile]   = GetFASTPar_Subfile(FP, 'InflowFile', fast.FAST_directory, fast.FAST_directory);
+    [ADP, ADFile]   = GetFASTPar_Subfile(FP, 'AeroFile', fast.FAST_directory, fast.FAST_directory);
+    [SvDP, SvDFile] = GetFASTPar_Subfile(FP, 'ServoFile', fast.FAST_directory, fast.FAST_directory);
+    [HDP, HDFile]   = GetFASTPar_Subfile(FP, 'HydroFile', fast.FAST_directory, fast.FAST_directory,true);
+    % [SbDP, SbDFile] = GetFASTPar_Subfile(FP, 'SubFile', fast.FAST_directory, fast.FAST_directory);
+    % [MDP, MDFile]   = GetFASTPar_Subfile(FP, 'MooringFile', fast.FAST_directory, fast.FAST_directory);
+    % [IDP, IDFile] = GetFASTPar_Subfile(FP, 'IceFile', fast.FAST_directory, fast.FAST_directory);
+    
+    % Get & Set Blade Files
+    % ED_BldFile              = GetFASTPar(EDP, 'BldFile1');
+    [ED_BldP, ED_BldFile]   = GetFASTPar_Subfile(EDP, 'BldFile1', fast.FAST_directory, fast.FAST_directory);
+    
+    % Get & Set Tower File
+    [ED_TwrP, ED_TwrFile]   = GetFASTPar_Subfile(EDP, 'TwrFile', fast.FAST_directory, fast.FAST_directory);
+    
+    % AeroDyn Blade
+    [AD_BldP, AD_BldFile]   = GetFASTPar_Subfile(ADP, 'ADBlFile(1)', fast.FAST_directory, fast.FAST_directory);
+    
+    % HydroData
+    % PotFile                 = GetFASTPar(HDP,'PotFile');
+    
+    % Airfoils
+    
+    % Control Parameters
+    [~,SD_dllFile]          = GetFASTPar_Subfile(SvDP,'DLL_InFile',fast.FAST_directory, fast.FAST_directory);
+    SD_dllP                 = ROSCO2Matlab(SD_dllFile,2);
+    
+    CpCtCqFile              = GetFASTPar(SD_dllP,'!PerfFileName');
+    %%% TODO: add processing here to look at/use Cx tables
+    
+    % MoorDyn: let's just copy for meow
+    MDFile = GetFASTPar(FP,'MooringFile');
+    
+    
+    
+    
+    
+    %% Write Outputs and Cleanup
+    
+    % parameters
+    P.FP        = FP;
+    P.EDP       = EDP;
+    P.IWP       = IWP;
+    P.ADP       = ADP;
+    P.HDP       = HDP;
+    P.SvDP      = SvDP;
+    P.ED_BldP   = ED_BldP;
+    P.ED_TwrP   = ED_TwrP;
+    P.AD_BldP   = AD_BldP;
+    P.SD_dllP   = SD_dllP;
+    
+    clearvars FP EDP IWP ADP HDP SvDP ED_BldP ED_TwrP AD_BldP SD_dllP
+    
+end
 %% Get OutData
 
 outfiles    = dir(fullfile(fast.FAST_directory,[fast.FAST_InputFile,'.out*']));
@@ -104,7 +105,7 @@ if POST_PROCESS
     % Channels = {'Wind1VelX','GenTq','BldPitch1','GenPwr','GenSpeed','RootMyb1','TwrBsMyt','PtfmPitch'};
     % outdata = PlotFASToutput([fast.FAST_runDirectory,filesep,fast.FAST_namingOut,'.out'],{'test'},1,Channels);
     
-
+    
     
     PLOT = 1;
     
